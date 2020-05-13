492c4925c908f60b1169c9b150bc69663cbd2846
mlch911 <mlch911>
2020-05-13 23:28:20 +0800

    Fix Bug: 获取v2ray版本改为使用jq

--- install.sh
+++ install.sh
@@ -808,10 +808,10 @@ caddy_config() {
 install_v2ray() {
 	$cmd update -y
 	if [[ $cmd == "apt-get" ]]; then
-		$cmd install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+		$cmd install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus jq
 	else
 		# $cmd install -y lrzsz git zip unzip curl wget qrencode libcap iptables-services
-		$cmd install -y lrzsz git zip unzip curl wget qrencode libcap
+		$cmd install -y lrzsz git zip unzip curl wget qrencode libcap jq
 	fi
 	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 	[ -d /etc/v2ray ] && rm -rf /etc/v2ray
